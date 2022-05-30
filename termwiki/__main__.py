c2e5d797d11938f367571e9c5c1ccd902a43b2e8
Gilad Barnea <giladbrn@gmail.com>
2022-05-30 23:17:52 +0300

    __main__.py now uses new cli.py file

--- termwiki/__main__.py
+++ termwiki/__main__.py
@@ -1,9 +1,3 @@
-# try:
-#     from rich.traceback import install
-#
-#     install(extra_lines=5, show_locals=True)
-# except ModuleNotFoundError as e:
-#     pass
 if __name__ == '__main__':
-    from . import main
-    main.get_page()
+    from termwiki import cli
+    cli.main()
