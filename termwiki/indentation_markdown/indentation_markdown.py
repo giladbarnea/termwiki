bfa7b272fb27c4140669596d38875017afd321c5
Gilad Barnea <giladbrn@gmail.com>
2022-06-02 23:59:53 +0300

    Block.first_line_index

--- termwiki/indentation_markdown/indentation_markdown.py
+++ termwiki/indentation_markdown/indentation_markdown.py
@@ -13,11 +13,16 @@ def first_truthy_line_index(lines: list) -> int:
 
 
 class Block:
-    def __init__(self, indent_level: int, parent: Block = None, lines: list[str] = None):
+    def __init__(self,
+                 indent_level: int,
+                 parent: Block = None,
+                 lines: list[str] = None,
+                 first_line_index: int = None):
         self.indent_level = indent_level
         self.children: deque[Block] = deque()
         self.parent: Block = parent
         self.lines = lines or []
+        self.first_line_index = first_line_index
 
     def __repr__(self):
         representation = f'Block(indent_level={self.indent_level}, lines={self.lines}'
@@ -40,7 +45,7 @@ class IndentationMarkdown:
         i = first_truthy_line_index(lines)
         first_line = lines[i].rstrip()
         indent_level = get_indent_level(first_line)
-        current_block = Block(indent_level, lines=[first_line])
+        current_block = Block(indent_level, lines=[first_line], first_line_index=i)
         self.blocks.append(current_block)
         i += 1
         while i < lines_count:
@@ -51,7 +56,10 @@ class IndentationMarkdown:
                 continue
             indent_level = get_indent_level(line)
             if indent_level > current_block.indent_level:
-                block = Block(indent_level=indent_level, parent=current_block, lines=[line])
+                block = Block(indent_level=indent_level,
+                              parent=current_block,
+                              lines=[line],
+                              first_line_index=i)
                 current_block.children.append(block)
                 current_block = block
             elif indent_level == current_block.indent_level:
