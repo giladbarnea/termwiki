d888ff9cbb9e679c10a14db376a1f3211f4aa8de
Gilad Barnea <giladbrn@gmail.com>
2022-09-26 18:04:27 +0300

    __init__.py terminal width height printing

--- termwiki/__init__.py
+++ termwiki/__init__.py
@@ -1,16 +1,27 @@
 # ** termwiki/__init__.py
 import os
+import sys
 from sys import path
 
 from rich.traceback import install
 from rich.console import Console
 console = Console(color_system='truecolor')
-print(f'{os.get_terminal_size() = }')
+try:
+    columns, rows = os.get_terminal_size()
+    print(f'{columns = }, {rows = }')
+except OSError as e:
+    print(repr(e),
+          f'{hasattr(sys.stderr, "isatty") = }',
+          f'{sys.stderr.isatty() = }',
+          f'{sys.stdout.isatty() = }',
+            sep='\n')
+    columns = os.getenv('COLUMNS', 130)
+    rows = os.getenv('LINES', 40)
 import click
 import bdb
 
 if home := os.path.expanduser('~') not in path:
     path.append(home)
-install(width=os.getenv('COLUMNS', 130), show_locals=True, suppress=(click, bdb))
+install(width=columns, show_locals=True, suppress=(click, bdb))
 
 from termwiki.page import page_tree
