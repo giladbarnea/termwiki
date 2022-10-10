e52cd9a1ba1a562ae178698b01a664155e61dbfa
Gilad Barnea <giladbrn@gmail.com>
2022-10-10 17:53:44 +0300

    fuzzy backends test and doc

---
+++ test/test_page/test_fuzzy_backend.py
@@ -0,0 +1,7 @@
+import pytest
+@pytest.skip
+def test_q_validators_matches_validator(fuzzy_find):
+    items = ["validator", "field", "config"]
+    initial_search = 'validators'
+    found = fuzzy_find(items, initial_search)
+    assert found == 'validator'
